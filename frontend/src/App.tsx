2f4f99d78765cedd42a6b39e8206e526b52ed9ab
Victor Seleimend <sseleimend@gmail.com>
2025-08-03 17:21:42 -0300

    Initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,24 @@
+import {
+  SignedOut,
+  SignInButton,
+  SignedIn,
+  UserButton,
+} from "@clerk/clerk-react";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <header>
+      <SignedOut>
+        <SignInButton>
+          <Button>Sign In</Button>
+        </SignInButton>
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </header>
+  );
+}
+
+export default App;
