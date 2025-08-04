a4756367d27fab1ae8e6d6cd17431d72118b5476
Victor Seleimend <sseleimend@gmail.com>
2025-08-04 09:09:25 -0300

    Add callback route for clerk

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,5 +1,8 @@
 import { Router } from "express";
+import { authCallback } from "../controllers/auth.controller.js";
 
 const router = Router();
+
+router.post("/callback", authCallback);
 
 export default router;
