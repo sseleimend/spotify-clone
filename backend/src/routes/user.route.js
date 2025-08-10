763c2e1d67d5cbe7f836603ea4e112263737883f
Victor Seleimend <sseleimend@gmail.com>
2025-08-10 13:03:26 -0300

    Add route for fetching all users with authentication protection

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,5 +1,9 @@
 import { Router } from "express";
+import { getAllUsers } from "../controllers/user.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = Router();
+
+router.get("/", protectRoute, getAllUsers);
 
 export default router;
