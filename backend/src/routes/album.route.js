11e7c438bc8ae0afd8c5dddffd607f48d9bd6076
Victor Seleimend <sseleimend@gmail.com>
2025-08-10 12:02:26 -0300

    Add album routes for fetching all albums and album by ID

--- backend/src/routes/album.route.js
+++ backend/src/routes/album.route.js
@@ -1,5 +1,9 @@
 import { Router } from "express";
+import { getAlbumById, getAllAlbums } from "../controllers/album.controller.js";
 
 const router = Router();
+
+router.get("/", getAllAlbums);
+router.get("/:id", getAlbumById);
 
 export default router;
