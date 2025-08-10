9f5df74383d81df165e6b652d4ac5c6ed2a6d47c
Victor Seleimend <sseleimend@gmail.com>
2025-08-10 18:40:47 -0300

    Add MainLayout component with resizable panels and sidebar

---
+++ frontend/src/layout/MainLayout.tsx
@@ -0,0 +1,37 @@
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
+import { Outlet } from "react-router";
+import { LeftSidebar } from "./components/LeftSidebar";
+
+export const MainLayout = () => {
+  const isMobile = false;
+
+  return (
+    <div className="min-h-screen bg-black text-white flex flex-col ">
+      <ResizablePanelGroup
+        direction="horizontal"
+        className="flex flex-1 h-full p-2 overflow-hidden"
+      >
+        <ResizablePanel defaultSize={20} minSize={isMobile ? 0 : 20}>
+          <LeftSidebar />
+        </ResizablePanel>
+        <ResizableHandle className="w-2 bg-black rounded-lg transition-colors" />
+        <ResizablePanel defaultSize={60} minSize={isMobile ? 80 : 60}>
+          <Outlet />
+        </ResizablePanel>
+        <ResizableHandle className="w-2 bg-black rounded-lg transition-colors" />
+        <ResizablePanel
+          defaultSize={20}
+          minSize={0}
+          maxSize={25}
+          collapsedSize={0}
+        >
+          <div className="p-4">Panel 2</div>
+        </ResizablePanel>
+      </ResizablePanelGroup>
+    </div>
+  );
+};
