e8be5ccdce6634b999e81586f34c026f55c837f3
Victor Seleimend <sseleimend@gmail.com>
2025-08-10 11:56:26 -0300

    Fix error handling in requireAdmin middleware to ensure errors are passed to the next middleware

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -20,5 +20,7 @@ export const requireAdmin = async (req, res, next) => {
         .json({ message: "Unauthorized - you must be an admin " });
 
     next();
-  } catch (error) {}
+  } catch (error) {
+    next(error);
+  }
 };
