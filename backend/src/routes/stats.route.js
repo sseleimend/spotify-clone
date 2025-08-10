1ca9e1663353264aca758fd46fcf687b35f3a1aa
Victor Seleimend <sseleimend@gmail.com>
2025-08-10 13:03:35 -0300

    Add route for fetching all statistics with admin protection

--- backend/src/routes/stats.route.js
+++ backend/src/routes/stats.route.js
@@ -1,5 +1,9 @@
 import { Router } from "express";
+import { protectRoute, requireAdmin } from "../middleware/auth.middleware.js";
+import { getAllStats } from "../controllers/stats.controller.js";
 
 const router = Router();
+
+router.get("/", protectRoute, requireAdmin, getAllStats);
 
 export default router;
