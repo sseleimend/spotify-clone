5252412ce3591ef91dc5eb09998736c6d16de07d
Victor Seleimend <sseleimend@gmail.com>
2025-08-10 18:40:38 -0300

    Refactor routing structure to include MainLayout and Chat page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,12 @@ import { Route, Routes } from "react-router";
 import { Home } from "./pages/home/Home";
 import { AuthCallback } from "./pages/auth-callback/AuthCallback";
 import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react";
+import { MainLayout } from "./layout/MainLayout";
+import { Chat } from "./pages/chat/Chat";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
       <Route
         path="/sso-callback"
         element={
@@ -14,6 +15,10 @@ function App() {
         }
       />
       <Route path="/auth-callback" element={<AuthCallback />} />
+      <Route element={<MainLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/chat" element={<Chat />} />
+      </Route>
     </Routes>
   );
 }
