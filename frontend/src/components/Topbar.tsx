ecd871257c489332a3fe7711d6f2624d5b37f8b0
Victor Seleimend <sseleimend@gmail.com>
2025-08-16 19:38:33 -0300

    Refactor Topbar component for improved structure and styling

--- frontend/src/components/Topbar.tsx
+++ frontend/src/components/Topbar.tsx
@@ -1,34 +1,39 @@
-import { SignedIn, SignedOut, SignOutButton } from "@clerk/clerk-react";
+import {
+  SignedIn,
+  SignedOut,
+  SignOutButton,
+  UserButton,
+} from "@clerk/clerk-react";
 import { LayoutDashboardIcon } from "lucide-react";
 import { Link } from "react-router";
 import { SignInOAuthButtons } from "./SignInOAuthButtons";
-import { Button } from "./ui/button";
+import { Button, buttonVariants } from "./ui/button";
+import useAuth from "@/stores/useAuth";
+import { cn } from "@/lib/utils";
 
 export const Topbar = () => {
-  const isAdmin = false;
+  const { isAdmin } = useAuth();
 
   return (
     <div className="flex items-center justify-between p-4 bg-zinc-900/75 backdrop-blur-md text-white sticky top-0 z-50">
       <div className="flex gap-2 items-center">
-        <h1 className="text-lg font-bold">Spotify Clone</h1>
+        <img src="/spotify.png" className="size-8" alt="Spotify Logo" />
+        <h1 className="text-lg">Spotify</h1>
       </div>
       <div className="flex items-center gap-4">
         {isAdmin && (
-          <Link to="/admin" className="px-4 py-2 rounded">
+          <Link
+            to="/admin"
+            className={cn(buttonVariants({ variant: "outline" }))}
+          >
             <LayoutDashboardIcon />
             Admin Panel
           </Link>
         )}
-        <SignedIn>
-          <SignOutButton>
-            <Button variant="secondary" className="px-4 py-2 rounded">
-              Logout
-            </Button>
-          </SignOutButton>
-        </SignedIn>
         <SignedOut>
           <SignInOAuthButtons />
         </SignedOut>
+        <UserButton />
       </div>
     </div>
   );
