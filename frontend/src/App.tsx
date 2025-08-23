498b5aac4b6d61143c21de84e2508ae2cef86dea
Victor Seleimend <sseleimend@gmail.com>
2025-08-23 14:51:51 -0300

    Refactor App component structure for improved readability and organization

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,26 +2,34 @@ import { Route, Routes } from "react-router";
 import { Home } from "./pages/home/Home";
 import { AuthCallback } from "./pages/auth-callback/AuthCallback";
 import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react";
-import { MainLayout } from "./layout/MainLayout";
+import MainLayout from "./layout/MainLayout";
 import { Chat } from "./pages/chat/Chat";
 import { Album } from "./pages/album/Album";
+import Admin from "./pages/admin/Admin";
+
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
-    <Routes>
-      <Route
-        path="/sso-callback"
-        element={
-          <AuthenticateWithRedirectCallback signInForceRedirectUrl="/auth-callback" />
-        }
-      />
-      <Route path="/auth-callback" element={<AuthCallback />} />
-      <Route element={<MainLayout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/chat" element={<Chat />} />
-        <Route path="/album/:albumId" element={<Album />} />
-      </Route>
-    </Routes>
+    <>
+      <Routes>
+        <Route
+          path="/sso-callback"
+          element={
+            <AuthenticateWithRedirectCallback signInForceRedirectUrl="/auth-callback" />
+          }
+        />
+        <Route path="/auth-callback" element={<AuthCallback />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/chat" element={<Chat />} />
+          <Route path="/album/:albumId" element={<Album />} />
+        </Route>
+      </Routes>
+
+      <Toaster />
+    </>
   );
 }
 
