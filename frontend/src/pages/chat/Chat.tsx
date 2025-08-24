a195b2d205a8560dfe832f8f00bff6672007483b
Victor Seleimend <sseleimend@gmail.com>
2025-08-24 12:44:09 -0300

    Implement chat functionality with user fetching and message retrieval

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -1,3 +1,28 @@
+import { Topbar } from "@/components/Topbar";
+import { useChat } from "@/stores/useChat";
+import { useUser } from "@clerk/clerk-react";
+import { useEffect } from "react";
+import UsersList from "./components/UsersList";
+
 export const Chat = () => {
-  return <div>Chat</div>;
+  const { user } = useUser();
+  const { messages, selectedUser, fetchUsers, fetchMessages } = useChat();
+
+  useEffect(() => {
+    if (user) fetchUsers();
+  }, [fetchUsers, user]);
+
+  useEffect(() => {
+    if (selectedUser) fetchMessages(selectedUser.clerkId);
+  }, [fetchMessages, selectedUser]);
+
+  return (
+    <main className="h-full rounded-lg bg-gradient-to-b from-zinc-800 to-zinc-900 overflow-hidden">
+      <Topbar />
+
+      <div className="grid lg:grid-cols-[300px_1fr] grid-cols-[80px_1fr] h-[calc(100vh-180px)]">
+        <UsersList />
+      </div>
+    </main>
+  );
 };
