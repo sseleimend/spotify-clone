584a1275a457592ed7d0f9a601757c188f4346c1
Victor Seleimend <sseleimend@gmail.com>
2025-08-24 12:43:48 -0300

    Add getMessages function to retrieve messages between users

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,3 +1,4 @@
+import { Message } from "../models/message.model.js";
 import { User } from "../models/user.model.js";
 
 export const getAllUsers = async (req, res, next) => {
@@ -10,6 +11,24 @@ export const getAllUsers = async (req, res, next) => {
       return res.status(404).json({ error: "No users found" });
     }
     res.status(200).json(users);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getMessages = async (req, res, next) => {
+  try {
+    const { userId } = req.params;
+    const currentUserId = req.auth.userId;
+
+    const messages = await Message.find({
+      $or: [
+        { senderId: userId, receiverId: currentUserId },
+        { receiverId: userId, senderId: currentUserId },
+      ],
+    }).sort({ createdAt: 1 });
+
+    res.status(200).json(messages);
   } catch (error) {
     next(error);
   }
