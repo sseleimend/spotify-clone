8f789cfacaed83fcadd381306ef0538bfd122cd2
Victor Seleimend <sseleimend@gmail.com>
2025-08-24 12:43:57 -0300

    Add route for retrieving messages between users

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { getAllUsers } from "../controllers/user.controller.js";
+import { getAllUsers, getMessages } from "../controllers/user.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = Router();
 
 router.get("/", protectRoute, getAllUsers);
+router.get("/messages/:userId", protectRoute, getMessages);
 
 export default router;
